2bf6d31f1783639d8bbcc6aee982cc02974d242f
Adolfo Jimenez <adolfoj@protonmail.com>
2023-04-20 18:25:19 -0400

    Add `fish` dotfiles

---
+++ fish/README.md
@@ -0,0 +1,10 @@
+## Required Symbolic Links
+
+``` fish
+ln -s $HOME/dotfiles/fish/conf.d $HOME/.config/fish/conf.d; \
+ln -s $HOME/dotfiles/fish/functions $HOME/.config/fish/functions; \
+ln -s $HOME/dotfiles/fish/config.fish $HOME/.config/fish/config.fish; \
+ln -s $HOME/dotfiles/fish/completions $HOME/.config/fish/completions; \
+ln -s $HOME/dotfiles/fish/fish_variables $HOME/.config/fish/fish_variables
+```
+
---
+++ fish/config.fish
@@ -0,0 +1,36 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Set default keybindings
+# fish_default_key_bindings
+fish_vi_key_bindings
+
+# Aliasses Setter
+alias setalias='read alias && echo "alias $alias" >> $HOME/.config/fish/config.fish'
+
+# Useful Aliasses
+alias cl='clear'
+alias nf='neofetch'
+
+# Git Aliasses
+alias gb='git branch'
+alias gco='git checkout'
+alias gs='clear && git status'
+alias gd='git diff'
+alias ga='git add'
+alias gaa='git add -A'
+alias gl='git log'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gp='git push'
+alias gd='git pull'
+
+alias gmd='git checkout main && git merge dev && git push && git checkout dev'
+alias gmdd='git push && git checkout main && git merge dev && git push && git checkout dev'
+
+# System Aliasses
+alias bat='python $HOME/workspace/testlab/linux-tools/get_power_consumption.py'
+alias stop-postgres='sudo systemctl stop postgresql && systemctl status postgresql | grep Active:'
+alias start-postgres='sudo systemctl start postgresql && systemctl status postgresql | grep Active:'
+
